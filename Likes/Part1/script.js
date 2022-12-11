050410aaedeb061a540a163344793d8c14f34f5d
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2022-12-11 15:08:21 -0800

    Add files via upload

---
+++ Likes/Part1/script.js
@@ -0,0 +1,8 @@
+
+var orignum = document.querySelector("#likeamount");
+var count = 3;
+
+function likesup(){
+    count++;
+    orignum.innerText = count;
+}
---
+++ Likes/Part2/script.js
@@ -0,0 +1,25 @@
+console.log("1234");
+
+var orignum1 = document.querySelector("#likeamount1");
+var count1 = 13;
+
+function likesup1(){
+    count1++;
+    orignum1.innerText = count1;
+}
+
+var orignum2 = document.querySelector("#likeamount2");
+var count2 = 200;
+
+function likesup2(){
+    count2++;
+    orignum2.innerText = count2;
+}
+
+var orignum3 = document.querySelector("#likeamount3");
+var count3 = 0;
+
+function likesup3(){
+    count3++;
+    orignum3.innerText = count3;
+}
